26dfb20ff01fbaec34088d4bfd6e29dad9023524
Hardik Menger <hardik.menger@gmail.com>
2018-06-24 22:36:00 +0530

    deployment

--- client/src/actions/authaction.js
+++ client/src/actions/authaction.js
@@ -6,7 +6,7 @@ import jwt_decode from "jwt-decode";
 //register user
 export const registeruser = (userdata, history) => dispatch => {
   axios
-    .post("http://localhost:3001/api/users/register", userdata)
+    .post("/api/users/register", userdata)
     .then(res => {
       history.push("/login");
     })
@@ -20,7 +20,7 @@ export const registeruser = (userdata, history) => dispatch => {
 };
 export const loginuser = (userdata, history) => dispatch => {
   axios
-    .post("http://localhost:3001/api/users/login", userdata)
+    .post("/api/users/login", userdata)
     .then(res => {
       //save token localstorage
       const { token } = res.data;
