37a6cecff4d8bcf76c302dcb5ab1beae00c90ef5
Hardik Menger <hardik.menger@gmail.com>
2018-06-24 20:48:18 +0530

    deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 var cors = require("cors");
-
+const path = require("path");
 //routes
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -36,5 +36,13 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`running server on  ${port}`));
